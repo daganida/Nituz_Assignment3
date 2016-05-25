bb9cbcb324affdd6690c2a79f001d846dbd43a84
idan dagan <daganida@post.bgu.ac.il>
2016-05-25 11:54:19 +0300

    Initial commit

---
+++ src/AirConditioner.java
@@ -0,0 +1,25 @@
+
+public interface AirConditioner {
+
+	//Turn on air conditioner
+		public boolean on();
+		//Turn off air conditioner	
+		public boolean off();
+		//Set air conditioner Mode - Hot/Cold
+		public void setMode(String mode);
+		//Set air conditioner temperature
+		public void setTemp(int temp);
+		//increase air conditioner temperature
+		public void incTemp(int temp);
+		//decrease air conditioner temperature
+		public void decTemp(int temp);
+		//get current air conditioner temperature
+		public int getCurrTemp();
+		//get current room temperature
+		public int getRoomTemp();
+		//increase room temperature
+		public void incRoomTemp(int temp);
+		//decrease room temperature
+		public void decRoomTemp(int temp);
+		
+}
---
+++ src/MyAirConditioner.java
@@ -0,0 +1,72 @@
+
+public class MyAirConditioner implements AirConditioner{
+
+	
+	public MyAirConditioner(){
+		
+	}
+
+	@Override
+	public boolean on() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean off() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setMode(String mode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTemp(int temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void incTemp(int temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void decTemp(int temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getCurrTemp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRoomTemp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void incRoomTemp(int temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void decRoomTemp(int temp) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
+
