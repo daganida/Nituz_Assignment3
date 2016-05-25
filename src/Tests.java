1255fe9175f930eb3781126a6e8875e3ab60075b
idan dagan <daganida@post.bgu.ac.il>
2016-05-25 15:39:39 +0300

    last_update

--- src/Tests.java
+++ src/Tests.java
@@ -4,6 +4,13 @@ public class Tests {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyAirConditioner airCond = new MyAirConditioner();
+		//
+		System.out.println("Hi");
+		//
+		///the new
+		//hello
+		//check
+		//hello
 	}
 
 }
